3f83c8dbb149a6fb999db80923b9754bf891ade4
Taewoo Kim <xodn4195@gmail.com>
2019-08-31 03:12:28 +0900

    add jest and enzyme for tests and status api spec

---
+++ pages/api/__tests__/status.spec.ts
@@ -0,0 +1,34 @@
+import statusApi from '../status';
+import {NextApiRequest, NextApiResponse} from 'next-server/dist/lib/utils';
+import {constants} from 'http2';
+
+const {
+  HTTP_STATUS_OK,
+  HTTP2_HEADER_CONTENT_TYPE
+} = constants;
+
+describe('Status api', function () {
+  it('should return 200 status and empty json body.', function () {
+    const req: jest.Mocked<NextApiRequest> = {} as any;
+    const res: jest.Mocked<NextApiResponse> = {
+      setHeader: jest.fn(),
+      status: jest.fn(),
+      end: jest.fn(),
+    } as any;
+
+    res.status.mockImplementation((_: number) => res);
+    statusApi(req, res);
+
+    const setHeaderMockCalls = res.setHeader.mock.calls;
+    expect(setHeaderMockCalls.length).toEqual(1);
+    expect(setHeaderMockCalls[0]).toEqual([ HTTP2_HEADER_CONTENT_TYPE, 'application/json']);
+
+    const statusMockCalls = res.status.mock.calls;
+    expect(statusMockCalls.length).toEqual(1);
+    expect(statusMockCalls[0][0]).toEqual(HTTP_STATUS_OK);
+
+    const endMockCalls = res.end.mock.calls;
+    expect(endMockCalls.length).toEqual(1);
+    expect(endMockCalls[0][0]).toEqual('{}');
+  });
+});
--- pages/api/status.ts
+++ pages/api/status.ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next-server/dist/lib/utils';
+import {constants} from 'http2';
+
+const {
+  HTTP2_HEADER_CONTENT_TYPE,
+  HTTP_STATUS_OK
+} = constants;
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).end('{}');
+  res.setHeader(HTTP2_HEADER_CONTENT_TYPE, 'application/json');
+  res.status(HTTP_STATUS_OK).end('{}');
 };
